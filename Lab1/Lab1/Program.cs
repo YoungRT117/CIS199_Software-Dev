d9c41bf84cd57ee8c6e3cdfe89acd24d138426fd
Roger Young Jr <77699485+YoungRT117@users.noreply.github.com>
2022-03-24 01:55:00 -0400

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Grading ID: R3555
+//Lab1
+//Due Date: 1/19/2020
+//Course Section: CIS 199-01-4202
+/*Brief Description: My program uses the WriteLine method, so each System.Console.WriteLine script
+ *displays a different message with is within the paratheses and quotes.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Grading ID:     R3555");
+            System.Console.WriteLine("Hobbies:        MMA and Gaming");
+            System.Console.WriteLine("Favorite Book:  Friday Night Lights");
+            System.Console.WriteLine("Favorite Movie: The Matrix");
+        }
+    }
+}
