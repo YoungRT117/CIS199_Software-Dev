6c5e3c53f05f368574aa187271940976eb3d5f2c
Roger Young Jr <77699485+YoungRT117@users.noreply.github.com>
2022-03-24 02:09:07 -0400

    Add files via upload

---
+++ Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab_2
+{
+    partial class Lab2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PriceOfMeal = new System.Windows.Forms.Label();
+            this.Tax15 = new System.Windows.Forms.Label();
+            this.Tax18 = new System.Windows.Forms.Label();
+            this.Tax20 = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.CalculateTip = new System.Windows.Forms.Button();
+            this.taxes15 = new System.Windows.Forms.Label();
+            this.taxes18 = new System.Windows.Forms.Label();
+            this.taxes20 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PriceOfMeal
+            // 
+            this.PriceOfMeal.AutoSize = true;
+            this.PriceOfMeal.Location = new System.Drawing.Point(158, 109);
+            this.PriceOfMeal.Name = "PriceOfMeal";
+            this.PriceOfMeal.Size = new System.Drawing.Size(198, 25);
+            this.PriceOfMeal.TabIndex = 0;
+            this.PriceOfMeal.Text = "Enter price of meal:";
+            // 
+            // Tax15
+            // 
+            this.Tax15.AutoSize = true;
+            this.Tax15.Location = new System.Drawing.Point(285, 166);
+            this.Tax15.Name = "Tax15";
+            this.Tax15.Size = new System.Drawing.Size(55, 25);
+            this.Tax15.TabIndex = 1;
+            this.Tax15.Text = "15%";
+            // 
+            // Tax18
+            // 
+            this.Tax18.AutoSize = true;
+            this.Tax18.Location = new System.Drawing.Point(285, 247);
+            this.Tax18.Name = "Tax18";
+            this.Tax18.Size = new System.Drawing.Size(55, 25);
+            this.Tax18.TabIndex = 2;
+            this.Tax18.Text = "18%";
+            // 
+            // Tax20
+            // 
+            this.Tax20.AutoSize = true;
+            this.Tax20.Location = new System.Drawing.Point(285, 325);
+            this.Tax20.Name = "Tax20";
+            this.Tax20.Size = new System.Drawing.Size(55, 25);
+            this.Tax20.TabIndex = 3;
+            this.Tax20.Text = "20%";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(362, 109);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(202, 31);
+            this.InputBox.TabIndex = 4;
+            // 
+            // CalculateTip
+            // 
+            this.CalculateTip.Location = new System.Drawing.Point(343, 399);
+            this.CalculateTip.Name = "CalculateTip";
+            this.CalculateTip.Size = new System.Drawing.Size(250, 58);
+            this.CalculateTip.TabIndex = 5;
+            this.CalculateTip.Text = "Calculate Tip";
+            this.CalculateTip.UseVisualStyleBackColor = true;
+            this.CalculateTip.Click += new System.EventHandler(this.CalculateTax_Click);
+            // 
+            // taxes15
+            // 
+            this.taxes15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxes15.Location = new System.Drawing.Point(362, 165);
+            this.taxes15.Name = "taxes15";
+            this.taxes15.Size = new System.Drawing.Size(202, 35);
+            this.taxes15.TabIndex = 6;
+            // 
+            // taxes18
+            // 
+            this.taxes18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxes18.Location = new System.Drawing.Point(362, 246);
+            this.taxes18.Name = "taxes18";
+            this.taxes18.Size = new System.Drawing.Size(202, 35);
+            this.taxes18.TabIndex = 7;
+            // 
+            // taxes20
+            // 
+            this.taxes20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxes20.Location = new System.Drawing.Point(362, 324);
+            this.taxes20.Name = "taxes20";
+            this.taxes20.Size = new System.Drawing.Size(202, 35);
+            this.taxes20.TabIndex = 8;
+            // 
+            // Lab2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 544);
+            this.Controls.Add(this.taxes20);
+            this.Controls.Add(this.taxes18);
+            this.Controls.Add(this.taxes15);
+            this.Controls.Add(this.CalculateTip);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.Tax20);
+            this.Controls.Add(this.Tax18);
+            this.Controls.Add(this.Tax15);
+            this.Controls.Add(this.PriceOfMeal);
+            this.Name = "Lab2";
+            this.Text = "Lab 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PriceOfMeal;
+        private System.Windows.Forms.Label Tax15;
+        private System.Windows.Forms.Label Tax18;
+        private System.Windows.Forms.Label Tax20;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button CalculateTip;
+        private System.Windows.Forms.Label taxes15;
+        private System.Windows.Forms.Label taxes18;
+        private System.Windows.Forms.Label taxes20;
+    }
+}
+
---
+++ Lab2/Lab2/Form1.cs
@@ -0,0 +1,44 @@
+﻿/*Grading ID: R3555
+Lab#:Lab 2
+Due Date: 02/02/2020
+Course Section: CIS 199-01
+Brief Description: The coding below is created so that when you click the "Calculate Tip" button, it
+                   show the amount given at a 15%, 18% and 20% tip. We defined the variables needed,
+                   converted the number inputed in the text box into a double, made variables to make it
+                   easier to apply the percentages using "const double", seen below and then we use a
+                   string.format line to display to results in their respective boxes in currency format.
+*/
+
+using System;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Lab2 : Form
+    {
+        public Lab2()
+        {
+            InitializeComponent();
+        }
+       
+        /*The event handler is initiated when you click on the "Calculate Tip" button, and description of
+         * how it work is in the brief description at the top! */
+        private void CalculateTax_Click(object sender, EventArgs e)
+        {
+            double tax1; //Variable for 15% tip 
+            double num1; //Variable for number inputted into the text box
+            double tax2; //Variable for 18% tip
+            double tax3; //Variable for 20% tip
+            num1 = Convert.ToDouble(InputBox.Text);
+            const double TIP_RATE1 = 0.15; //Variable to define 15% to be used arithmatically 
+            const double TIP_RATE2 = 0.18; //Variable to define 18% to be used arithmatically 
+            const double TIP_RATE3 = 0.20; //Variable to define 20% to be used arithmatically 
+            tax1 = num1 * TIP_RATE1;
+            tax2 = num1 * TIP_RATE2;
+            tax3 = num1 * TIP_RATE3;
+            taxes15.Text = string.Format("{0:C}", tax1);
+            taxes18.Text = string.Format("{0:C}", tax2);
+            taxes20.Text = string.Format("{0:C}", tax3);
+        }
+    }
+}
